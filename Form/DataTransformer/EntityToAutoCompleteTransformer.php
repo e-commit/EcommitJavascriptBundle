9d0105768654620c2ee80476ac4e26785b3ce275
hlecorche <hlecorche@e-commit.fr>
2011-05-31 10:58:33 +0200

    Ajout field form "entity_autocomplete"

---
+++ Form/DataTransformer/EntityToAutoCompleteTransformer.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the EcommitJavascriptBundle package.
+ *
+ * (c) Hubert LECORCHE <hlecorche@e-commit.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ecommit\JavascriptBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Doctrine\ORM\QueryBuilder;
+
+class EntityToAutoCompleteTransformer implements DataTransformerInterface
+{
+    protected $query_builder;
+    protected $alias;
+    protected $method;
+    protected $key_method;
+    
+    /**
+     * Constructor
+     * 
+     * @param QueryBuilder $query_builder
+     * @param string $alias     Alias to search query
+     * @param string $method    Method to get displayed value
+     * @param sting $key_method Method to get identifier
+     */
+    public function __construct(QueryBuilder $query_builder, $alias, $method, $key_method)
+    {
+        $this->query_builder = $query_builder;
+        $this->alias = $alias;
+        $this->method = $method;
+        $this->key_method = $key_method;
+    }
+    
+    /**
+     * Transforms entity to array (key, text)
+     * 
+     * @param Object $entity
+     * @return Array 
+     */
+    public function transform($entity)
+    {
+        if (null === $entity || '' === $entity)
+        {
+            return array('key' => '', 'text' => '');
+        }
+        
+        if (!is_object($entity))
+        {
+            throw new UnexpectedTypeException($entity, 'object');
+        }
+        
+        $key_method = $this->key_method;
+        $method = $this->method;
+        $key = $entity->$key_method();
+        $text = $entity->$method();
+        return array('key' => $key, 'text' => $text); 
+    }
+    
+    /**
+     * Tranforms array (key, text) to entity
+     * 
+     * @param Array $value
+     * @return Object 
+     */
+    public function reverseTransform($value)
+    {
+        $key = (isset($value['key']))? $value['key'] : null;
+        
+        if ('' === $key || null === $key)
+        {
+            return null;
+        }
+        
+        if(!is_scalar($key))
+        {
+            throw new TransformationFailedException('Value is not scalar');
+        }
+        
+        try
+        {
+            $query = $this->query_builder->andWhere(sprintf('%s = :key_transformer', $this->alias))
+            ->setParameter('key_transformer', $key)
+            ->getQuery();
+            
+            $entity = $query->getSingleResult();
+        }
+        catch(\Exception $e)
+        {
+            throw new TransformationFailedException(sprintf('The entity with key "%s" could not be found', $key));
+        }
+        return $entity;
+    }
+}
---
+++ Form/Type/EntityAutoCompleteType.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * This file is part of the EcommitJavascriptBundle package.
+ *
+ * (c) Hubert LECORCHE <hlecorche@e-commit.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ecommit\JavascriptBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\Exception\FormException;
+use Doctrine\ORM\EntityManager;
+use Ecommit\JavascriptBundle\jQuery\Manager;
+use Ecommit\JavascriptBundle\Form\DataTransformer\EntityToAutoCompleteTransformer;
+
+class EntityAutoCompleteType extends AbstractType
+{
+    protected $javascript_manager;
+    protected $em;
+    
+    /**
+     * Constructor
+     * 
+     * @param Manager $javascript_manager
+     * @param EntityManager $em
+     */
+    public function __construct(Manager $javascript_manager, EntityManager $em)
+    {
+        $this->javascript_manager = $javascript_manager;
+        $this->em = $em;
+    }
+    
+    
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder->add('key', 'hidden');
+        $builder->add('text', 'text');
+        
+        $required_options = array('url', 'alias');
+        foreach($required_options as $required_option)
+        {
+            if(empty($options[$required_option]))
+            {
+                throw new FormException(sprintf('The "%s" option is required', $required_option));
+            }
+        }
+        
+        if(!empty($options['query_builder']))
+        {
+            $query_builder = $options['query_builder'];
+            $alias = $options['alias'];
+        }
+        elseif(!empty($options['class']))
+        {
+            $query_builder = $this->em->createQueryBuilder()
+            ->from($options['class'], 'c')
+            ->select('c');
+            $alias = 'c.'.$options['alias'];
+        }
+        else
+        {
+            throw new FormException('"query_builder" or "class" option is required');
+        }
+        
+        $builder->appendClientTransformer(new EntityToAutoCompleteTransformer($query_builder, $alias, $options['method'], $options['key_method']));
+        
+        $builder->setAttribute('url', $options['url']);
+        $builder->setAttribute('image_autocomplete', $options['image_autocomplete']);
+        $builder->setAttribute('image_ok', $options['image_ok']);
+        $builder->setAttribute('min_length', $options['min_length']);
+    }
+
+    
+    public function buildView(FormView $view, FormInterface $form)
+    {
+        $this->javascript_manager->enablejQueryUi();
+        
+        $view->set('url', $form->getAttribute('url'));
+        $view->set('image_autocomplete', $form->getAttribute('image_autocomplete'));
+        $view->set('image_ok', $form->getAttribute('image_ok'));
+        $view->set('min_length', $form->getAttribute('min_length'));
+    }
+    
+    
+    public function getParent(array $options)
+    {
+        return 'form';
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'url'               => null,
+            'em'                => $this->em,
+            'class'             => null,
+            'query_builder'     => null,
+            'alias'             => null,
+            'method'            => '__toString',
+            'key_method'        => 'getId',
+            'image_autocomplete'=> '/bundles/ecommitcrud/images/i16/keyboard_magnify.png',
+            'image_ok'          => '/bundles/ecommitcrud/images/i16/apply.png',
+            'min_length'        => 2,
+            
+            'error_bubbling'    => false,
+        );
+    }
+
+    public function getName()
+    {
+        return 'entity_autocomplete';
+    }
+}
