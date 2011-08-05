d73b9febb60266df88ed587ae093527d6ffab121
hlecorche <hlecorche@e-commit.fr>
2011-08-05 11:20:29 +0200

    Modifications pour version SF 2.0

--- Listener/JavascriptListener.php
+++ Listener/JavascriptListener.php
@@ -35,7 +35,7 @@ class JavascriptListener
      *
      * @param FilterResponseEvent $event The notified event
      */
-    public function onCoreResponse(FilterResponseEvent $event)
+    public function onKernelResponse(FilterResponseEvent $event)
     {
         if (HttpKernelInterface::MASTER_REQUEST !== $event->getRequestType())
         {
