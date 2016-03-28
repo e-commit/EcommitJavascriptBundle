f8ed93968b6adc9ed7783d60d4a35c8dfd01a251
hlecorche <hlecorche@e-commit.fr>
2016-03-28 15:50:38 +0200

    Add Select2LanguageType

---
+++ Form/Type/Select2/Select2LanguageType.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the EcommitJavascriptBundle package.
+ *
+ * (c) E-commit <contact@e-commit.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ecommit\JavascriptBundle\Form\Type\Select2;
+
+use Symfony\Component\Form\Extension\Core\Type\LanguageType;
+
+class Select2LanguageType extends AbstractSelect2Type
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return LanguageType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'ecommit_javascript_select2language';
+    }
+}
